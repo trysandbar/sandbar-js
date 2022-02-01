84a7501a31a231b1c63b307cc7a4bf17a83fba64
Ryan Rhee <ryan.y.rhee@gmail.com>
2022-02-01 18:26:22 -0500

    [ingestion] Return results when submitting events

--- src/client.ts
+++ src/client.ts
@@ -1,11 +1,38 @@
 import * as publicapi from "./generated/sandbar"
 import * as grpc from "./generated/private/sandbar"
-import { JsonValue } from "@protobuf-ts/runtime"
 import fetch from "cross-fetch"
 import methodPaths from "./generated/private/method-paths"
 
 function base64encode(input: string) {
   Buffer.from(input, "utf8").toString("base64")
+}
+
+type EventResponse = {
+  sandbarId: string
+  isSuccessful: boolean
+  message: string
+} & (
+  | {
+      responseType: "entity"
+      sourceEntityId: string
+      generatedId: string
+    }
+  | {
+      responseType: "account"
+      sourceAccountId: publicapi.AccountIdentifier
+    }
+  | {
+      responseType: "accountEntityLink"
+    }
+  | {
+      responseType: "transaction"
+      sourceTransactionId: string
+    }
+)
+
+interface SubmitEventsResponse {
+  message: string
+  responses: EventResponse[]
 }
 
 class Client {
@@ -17,18 +44,24 @@ class Client {
     }
   ) {}
 
-  async submitEvents(events: publicapi.Event[]) {
+  async submitEvents(events: publicapi.Event[]): Promise<SubmitEventsResponse> {
     const req: publicapi.SubmitEventsRequest = {
       events,
     }
-    const json = grpc.SubmitEventsRequest.toJson(req)
+    const json = grpc.SubmitEventsRequest.toJsonString(req)
     const path = methodPaths.SubmitEvents
-    await this.post(path, json)
+    const response = await this.post(path, json)
+    const parsedResponse = grpc.SubmitEventsResponse.fromJsonString(response)
+    const { message, responses: grpcResponses } = parsedResponse
+    const responses = grpcResponses.map(translateResponse)
+    return {
+      message,
+      responses,
+    }
   }
 
-  private async post(path: string, json: JsonValue) {
+  private async post(path: string, body: string) {
     const url = new URL(path, this.base)
-    const body = json === null ? null : JSON.stringify(json)
     const authHeaders = this.getAuthHeaders()
     const response = await fetch(url.toString(), {
       method: "POST",
@@ -39,7 +72,8 @@ class Client {
       },
       body,
     })
-    return response.json()
+
+    return response.text()
   }
 
   private getAuthHeaders(): { Authorization?: string } {
@@ -55,4 +89,58 @@ class Client {
   }
 }
 
-export { Client }
+function translateResponse(response: grpc.EventResponse): EventResponse {
+  const {
+    sourceId,
+    isSuccessful,
+    sandbarId,
+    message,
+    generatedId,
+    eventResponseType,
+  } = response
+  switch (eventResponseType) {
+    case publicapi.EventResponseType.ENTITY:
+      return {
+        responseType: "entity",
+        sourceEntityId: sourceId,
+        isSuccessful,
+        sandbarId,
+        message,
+        generatedId,
+      }
+    case publicapi.EventResponseType.ACCOUNT: {
+      const [bankName, accountNumber] = sourceId.split("|", 2)
+      return {
+        responseType: "account",
+        sourceAccountId: {
+          bankName,
+          accountNumber,
+        },
+        isSuccessful,
+        sandbarId,
+        message,
+      }
+    }
+    case publicapi.EventResponseType.ACCOUNT_ENTITY_LINK:
+      return {
+        responseType: "accountEntityLink",
+        isSuccessful,
+        sandbarId,
+        message,
+      }
+    case publicapi.EventResponseType.TRANSACTION:
+      return {
+        responseType: "transaction",
+        sourceTransactionId: sourceId,
+        isSuccessful,
+        sandbarId,
+        message,
+      }
+    case publicapi.EventResponseType.UNSPECIFIED:
+      throw new TypeError(
+        "API contract violation: event response type unspecified"
+      )
+  }
+}
+
+export { Client, EventResponse, SubmitEventsResponse }
--- src/tests/sandbar.test.ts
+++ src/tests/sandbar.test.ts
@@ -1,18 +1,22 @@
+import { assert } from "console"
 import fetchMock from "jest-fetch-mock"
 jest.setMock("cross-fetch", fetchMock)
 
 import * as sandbar from "../"
+import * as grpc from "../generated/private/sandbar"
 
 beforeEach(() => {
   fetchMock.resetMocks()
 })
 
-test("id string with numeric succeeds", async () => {
+test("well-formed submit request succeeds", async () => {
   const api = new sandbar.Client()
   fetchMock.mockIf(
     /https:\/\/api\.sandbar\.ai\/v0\/submit_event\/?/,
     async (_req) => {
-      return JSON.stringify({ status: "success" })
+      return JSON.stringify({
+        message: "success",
+      })
     }
   )
   await api.submitEvents([
@@ -69,4 +73,132 @@ test("id string with non-numeric fails", async () => {
     SyntaxError,
     `Cannot convert ${alphaId} to a BigInt`
   )
+})
+
+test("creat new entities, accounts, links, and transaction", async () => {
+  const resp: grpc.SubmitEventsResponse = {
+    message: "success",
+    responses: [
+      {
+        eventResponseType: sandbar.EventResponseType.ENTITY,
+        sandbarId: "1",
+        sourceId: "test-person",
+        isSuccessful: true,
+        message: "",
+        generatedId: "",
+      },
+      {
+        eventResponseType: sandbar.EventResponseType.ACCOUNT,
+        sandbarId: "1",
+        sourceId: "Lendbar Bank|1234",
+        isSuccessful: true,
+        message: "",
+        generatedId: "",
+      },
+      {
+        eventResponseType: sandbar.EventResponseType.ACCOUNT_ENTITY_LINK,
+        sandbarId: "1",
+        sourceId: "",
+        isSuccessful: true,
+        message: "",
+        generatedId: "",
+      },
+      {
+        eventResponseType: sandbar.EventResponseType.TRANSACTION,
+        sandbarId: "1",
+        sourceId: "abc",
+        isSuccessful: true,
+        message: "",
+        generatedId: "",
+      },
+    ],
+  }
+  fetchMock.mockIf(
+    /https:\/\/api\.sandbar\.ai\/v0\/submit_event\/?/,
+    async (_req) => {
+      return JSON.stringify(resp)
+    }
+  )
+  const api = new sandbar.Client()
+  const result = await api.submitEvents([
+    {
+      type: sandbar.EventType.CREATE,
+      incomplete: false,
+      payload: {
+        oneofKind: "entity",
+        entity: {
+          sourceEntityId: "test-person",
+          name: "Test Person",
+          birthIncorporationDate: "01/01/1980",
+        },
+      },
+    },
+    {
+      type: sandbar.EventType.CREATE,
+      incomplete: false,
+      payload: {
+        oneofKind: "account",
+        account: {
+          accountIdentifier: {
+            accountNumber: "1234",
+            bankName: "Lendbar Bank",
+          },
+          accountType: sandbar.AccountType.CHECKING,
+        },
+      },
+    },
+    {
+      type: sandbar.EventType.CREATE,
+      incomplete: false,
+      payload: {
+        oneofKind: "accountEntityLink",
+        accountEntityLink: {
+          accountId: {
+            oneofKind: "sourceAccountIdentifier",
+            sourceAccountIdentifier: {
+              accountNumber: "1234",
+              bankName: "Lendbar Bank",
+            },
+          },
+          entityId: {
+            entityId: {
+              oneofKind: "sourceEntityId",
+              sourceEntityId: "test-person",
+            },
+          },
+          startDate: "01/01/2020",
+        },
+      },
+    },
+    {
+      type: sandbar.EventType.CREATE,
+      incomplete: false,
+      payload: {
+        oneofKind: "transaction",
+        transaction: {
+          sourceTransactionId: "abc",
+          transactionAmount: 20,
+          transactionCurrency: "usd",
+          accountIdentifier: {
+            accountNumber: "1234",
+            bankName: "Lendbar Bank",
+          },
+          transactionSourceEntityId: "test-person",
+          transactionType: sandbar.TransactionType.DEPOSIT,
+          isCredit: true,
+          executeTransactionDateTime: "",
+          productType: sandbar.ProductType.CREDIT_CARD,
+        },
+      },
+    },
+  ])
+  const { responses } = result
+  expect(responses).toHaveLength(4)
+  const accountResponse = responses[1]
+  expect(accountResponse.responseType).toBe("account")
+  if (accountResponse.responseType === "account") {
+    const { accountNumber, bankName } = accountResponse.sourceAccountId
+    expect(accountNumber).toBe("1234")
+    expect(bankName).toBe("Lendbar Bank")
+  }
 })
