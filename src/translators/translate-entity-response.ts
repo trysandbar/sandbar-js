87fb8b2f7257083688deabbb0839e072a4182b0e
Ryan Rhee <ryan.y.rhee@gmail.com>
2022-02-02 18:05:18 -0500

    [method] Implement GetEntities

---
+++ src/translators/translate-entity-response.ts
@@ -0,0 +1,50 @@
+import * as grpc from "../generated/private/sandbar"
+import * as publicapi from "../generated/sandbar"
+
+type RequireKeys<T, TKeys extends keyof T> = Omit<T, TKeys> &
+  Required<Pick<T, TKeys>>
+
+type CompleteEntity = RequireKeys<
+  publicapi.Entity,
+  | "sandbarEntityId"
+  | "sourceEntityId"
+  | "relationshipBeginDate"
+  | "name"
+  | "birthIncorporationDate"
+>
+
+export type Entity =
+  | ({ isGenerated: false } & CompleteEntity)
+  | ({ isGenerated: true } & publicapi.Entity)
+
+export function translateEntityResponse(entityGrpc: grpc.Entity): Entity {
+  const {
+    sandbarEntityId,
+    sourceEntityId,
+    relationshipBeginDate,
+    name,
+    birthIncorporationDate,
+  } = entityGrpc
+
+  const isGenerated = !isCompleteEntity(entityGrpc)
+  return isGenerated
+    ? { isGenerated, ...entityGrpc }
+    : { isGenerated, ...entityGrpc }
+}
+
+function isCompleteEntity(entity: grpc.Entity): entity is CompleteEntity {
+  const {
+    sandbarEntityId,
+    sourceEntityId,
+    relationshipBeginDate,
+    name,
+    birthIncorporationDate,
+  } = entity
+  return (
+    sandbarEntityId !== undefined &&
+    sourceEntityId !== undefined &&
+    relationshipBeginDate !== undefined &&
+    name !== undefined &&
+    birthIncorporationDate != undefined
+  )
+}
